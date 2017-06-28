229a8375eb3c3cbde5ff266b1830ddada7bea2c9
王树权 <3346148964@qq.com>
2017-06-28 15:22:44 +0800

    frist

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by webapps on 2017/6/28.
+//  Copyright © 2017年 wsq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by webapps on 2017/6/28.
+//  Copyright © 2017年 wsq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
